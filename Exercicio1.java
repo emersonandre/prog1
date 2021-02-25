1a8ed52335de8d123a46405a080d6b182935be65
Emerson Silvestrin <emersonsilvestrin@live.com>
2021-02-25 17:07:01 -0300

    Lista exercicio 1: Faça um programa em Java que imprima o seguinte na tela

---
+++ Exercicio1.java
@@ -0,0 +1,14 @@
+import jdk.tools.jlink.internal.SymLinkResourcePoolEntry;
+
+public class Exercicio1 {
+    public static void main(String[] args){
+        System.out.println("- Professor: GEOMAR ANDRE SCHREINER");
+        System.out.println("- Professor: ANDREI DE ALMEIDA SAMPAIO BRAGA");
+        System.out.println("- Aluno: Emerson Andre Silvestrin");
+        System.out.println("- Objetivo:");
+            System.out.println("       O objetivo dessa disciplina é compreender os conceitos fundamentais");
+            System.out.println("       do paradigma de programação orientada a objetos e aplicá-los no");
+            System.out.println("       desenvolvimento de soluções de software.");
+        System.out.println("- Turno: Vespertino");
+    }
+}
