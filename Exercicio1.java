f73344ba99a908de9c8032d7bda0801bfc6586ae
Emerson Silvestrin <emersonsilvestrin@live.com>
2021-02-26 08:25:15 -0300

    Imprimir - Lista 1 - Exercicio 1

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,3 +1,15 @@
+/*
+1) Faça um programa em Java que imprima o seguinte na tela:
+Programação I
+- Professor: Fernando Bevilacqua
+- Aluno: Seu nome aqui
+- Objetivo:
+O objetivo dessa disciplina é compreender os conceitos fundamentais
+do paradigma de programação orientada a objetos e aplicá-los no
+desenvolvimento de soluções de software.
+- Turno: Vespertino
+*/
+
 public class Exercicio1 {
     public static void main(String[] args){
         System.out.println("- Professor: GEOMAR ANDRE SCHREINER");
