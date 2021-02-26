dfd97a135306e0f538e3180e12665278ccdddfa8
Emerson Silvestrin <emersonsilvestrin@live.com>
2021-02-26 08:07:41 -0300

    Tabuada 6 e 2 - Lista 1 - exercico 4

---
+++ Exercico4.java
@@ -0,0 +1,9 @@
+public class Exercico4 {
+    public static void main(String[] args){
+        int tabuada6 = 6 ,tabuada2 = 2 ,x,y; // tabuada do 5
+        for(x = 1; x <= 10; x++){
+            System.out.print(x + " x " + tabuada6 + " = " + x * tabuada6 + "\t");
+            System.out.print(x + " x " + tabuada2 + " = " + x * tabuada2 + "\n"); 
+        }
+    }
+}
