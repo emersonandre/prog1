b3eac2a41e755d89d58e97bc59a0484c6e30140f
Emerson Silvestrin <emersonsilvestrin@live.com>
2021-02-26 08:07:19 -0300

    Tabuada 5 - Lista 1 - Exercicio 3

---
+++ Exercico3.java
@@ -0,0 +1,9 @@
+public class Exercico3 {
+    public static void main(String[] args){
+        int tabuada = 5,x; // tabuada do 5
+        for(x = 1; x <= 10; x++){
+            System.out.print(x + " x " + tabuada + " = ");
+            System.out.println(x * tabuada);
+        }
+    }
+}
