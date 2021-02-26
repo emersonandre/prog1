5351933da9f5d37204577890345c9d294a817f33
Emerson Silvestrin <emersonsilvestrin@live.com>
2021-02-26 08:29:05 -0300

    Tabuada 6 e 2 - Lista 1 Exercico 4

--- Exercico4.java
+++ Exercico4.java
@@ -1,3 +1,16 @@
+/*
+Faça um programa em Java que imprima na tela a tabuada do número 6 e do número 2, lado a lado, no seguinte Formato:
+1 x 6 = 6       1 x 2 = 2
+2 x 6 = 12      2 x 2 = 4
+3 x 6 = 18      3 x 2 = 6
+4 x 6 = 24      4 x 2 = 8
+5 x 6 = 30      5 x 2 = 10
+6 x 6 = 36      6 x 2 = 12
+7 x 6 = 42      7 x 2 = 14
+8 x 6 = 48      8 x 2 = 16
+9 x 6 = 54      9 x 2 = 18
+10 x 6 = 60     10 x 2 = 20
+*/
 public class Exercico4 {
     public static void main(String[] args){
         int tabuada6 = 6 ,tabuada2 = 2 ,x,y; // tabuada do 5
